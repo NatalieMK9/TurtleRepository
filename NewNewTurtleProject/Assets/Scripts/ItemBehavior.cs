d2a45de94553ed0169e7df1657e0e935a9b3b259
NatalieMK9 <64727532+NatalieMK9@users.noreply.github.com>
2021-02-18 23:42:14 -0500

    TurtleTest!

---
+++ NewNewTurtleProject/Assets/Scripts/ItemBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBehavior : MonoBehaviour
+{
+    void Start()
+    {
+    }
+
+    //when something collides to a pizza gameObject
+    void OnCollisionEnter(Collision collision)
+    {
+        //if the player is colliding to the pizza
+        if (collision.gameObject.name == "Turtle_Model")
+        {
+            //destroy the object
+            Destroy(this.gameObject);
+        }
+    }
+}
